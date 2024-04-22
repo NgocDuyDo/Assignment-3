4543139448fedc31ccffe0026c909284a8e0677e
ChohwiPark <chohwi.park-1@student.uts.edu.au>
2024-04-22 14:47:11 +1000

    create Xcode file

---
+++ assignment3/assignment3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  assignment3
+//
+//  Created by Chohwi Park on 22/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ assignment3/assignment3/assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment3App.swift
+//  assignment3
+//
+//  Created by Chohwi Park on 22/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
