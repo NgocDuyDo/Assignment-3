3ab687532ec08d64b6286e29175b52b7586cd58e
NgocDuyDo <163801386+NgocDuyDo@users.noreply.github.com>
2024-05-11 00:05:38 +1000

    Update MealLogView.swift

--- assignment3/assignment3/MealLogView.swift
+++ assignment3/assignment3/MealLogView.swift
@@ -6,11 +6,11 @@
 //
 
 import SwiftUI
-
-struct MealLogView: View {
-    @StateObject var viewModel = MealLogViewModel()
-    @State private var showingMealDetails = false
-    @State private var addingNewMeal = false
+ 
+ struct MealLogView: View {
+     @StateObject var viewModel = MealLogViewModel()
+     @State private var showingMealDetails = false
+     @State private var addingNewMeal = false
     
     var body: some View {
         NavigationView {
@@ -25,6 +25,15 @@ struct MealLogView: View {
                             .font(.headline)
                         Text("Meal Type: \(meal.mealType.rawValue.capitalized)")
                         Text("Calories: \(meal.calories)")
+                        if meal.hasMedication {
+                            Text("Medication Details:").foregroundStyle(.brown)
+                            Text("Medication Name: \(meal.medication!.name)")
+                            Text("Dosage: \(meal.medication!.dosage)")
+                            if meal.hasMedication, let medicationTime = meal.medication?.time{
+                                Text("Medication Time: \(medicationTime, formatter:DateFormatter.shortTime)")
+                            }
+                            Text("ReminderTiming: \(meal.medication!.reminderTiming.rawValue)")
+                        }
                     }
                     .padding()
                     .onTapGesture {
@@ -55,7 +64,7 @@ struct MealEntryView: View {
     @State private var medicationName = ""
     @State private var medicationDosage = ""
     @State private var selectedMedicationTime = Date()
-    @State private var selectedReminderTiming = 0
+    @State private var selectedReminderTimingIndex = 0
     let reminderTimings = ["Before Meal", "During Meal", "After Meal"]
     var body: some View {
         NavigationView {
@@ -69,16 +78,20 @@ struct MealEntryView: View {
                 TextField("Menu Name", text: $meal.menuName)
                 TextField("Calories", value: $meal.calories, formatter: NumberFormatter())
                 Section(header: Text("Medication Details")) {
-                    TextField("Medication name", text: $medicationName)
+                    TextField("Medication Name", text: $medicationName)
                     TextField("Dosage", text: $medicationDosage)
-                    DatePicker("Reminder Time", selection: $selectedMedicationTime, displayedComponents: .hourAndMinute)
-                    Picker("Reminder Timing", selection: $selectedReminderTiming) {
+                    DatePicker("Medication Time", selection: $selectedMedicationTime, displayedComponents: .hourAndMinute)
+                    Picker("Reminder Timing", selection: $selectedReminderTimingIndex){
                         ForEach(0..<reminderTimings.count) {index in
                             Text(reminderTimings[index])
                         }
                     }
                 }
+    
                 Button("Save") {
+                    let reminderTiming = reminderTimings[selectedReminderTimingIndex]
+                    let medication = Medication(name: medicationName, dosage: medicationDosage, time: selectedMedicationTime, reminderTiming: .afterMeal)
+                    meal.medication = medication
                     if let index = viewModel.meals.firstIndex(where: { $0.id == meal.id }) {
                         viewModel.meals[index] = meal // Update existing meal
                     } else {
@@ -93,13 +106,23 @@ struct MealEntryView: View {
 }
 
 
+
 extension MealLogViewModel {
     func hasMeals(for date: Date) -> Bool {
         !meals(for: date).isEmpty
     }
 }
-
-
-#Preview {
-    MealLogView()
+extension DateFormatter {
+    static let shortTime: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter
+    } ()
 }
+
+struct MealLogView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealLogView()
+    }
+}
+
